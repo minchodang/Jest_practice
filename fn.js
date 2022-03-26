8bdb2a24eacee4af91880876c6ded6a2d258c27d
minsukang <minsu910725@gmail.com>
2022-03-26 15:22:06 +0900

    Add: matcher test

--- fn.js
+++ fn.js
@@ -1,5 +1,9 @@
 const fn = {
   add: (num1, num2) => num1 + num2,
+  makeUser: (name, age) => ({ name, age, gender: undefined }),
+  throwErr: () => {
+    throw new Error('xx');
+  },
 };
 
 module.exports = fn;
--- fn.test.js
+++ fn.test.js
@@ -1,13 +1,5 @@
 const fn = require('./fn');
 
-test('1은 1이야.', () => {
-  expect(1).toBe(1);
-});
-
-test('2 더하기 3은 5야.', () => {
-  expect(fn.add(2, 3)).toBe(5);
-});
-
-test('3 더하기 3은 5가 아니야.', () => {
-  expect(fn.add(3, 3)).not.toBe(5);
+test('이거 에러 나나요?', () => {
+  expect(() => fn.throwErr()).toThrow('xx');
 });
